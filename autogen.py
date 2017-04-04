bc287dbca51c443c8e52e3658de856d3b720a8f1
Harry Hughes <hhughe11@Tingxings-MacBook-Pro.local>
2017-04-04 15:18:36 -0400

    autogen.py now creates CMakeLists. More options were added to wrap.xml. As of now, both above and below wrapper functions must be .c or .cpp. You can't mix and match yet

--- autogen.py
+++ autogen.py
@@ -1,4 +1,5 @@
 import xml.etree.ElementTree as xml
+import subprocess
 from pathlib import Path
 import argparse
 import sys
@@ -33,21 +34,41 @@ class wrap_class:
         self.below = wrap_below
 
 class wrap_func:
-    def __init__(self, name, arg_list):
+    def __init__(self, name, arg_list, filex, decl):
         self.name = name
         self.arg_list = arg_list
+        self.filex = filex
+        self.decl = decl
 
 #Each element of the arg list contains 'None' or a dictionary that tells the value of
 #the positional argument for the above or below wrapper depending on the core kernel
 #in question
 class config_class:
-    def __init__(self, above_func, above_args, below_func, below_args, use_default, default_color_map):
+    def __init__( \
+                 self, \
+                 above_func, \
+                 above_args, \
+                 above_filex, \
+                 above_decl, \
+                 below_func, \
+                 below_args, \
+                 below_filex, \
+                 below_decl, \
+                 use_default, \
+                 default_color_map, \
+                 trace_h \
+                 ):
         self.wrap_above_func = above_func
         self.wrap_below_func = below_func
         self.below_arg_list = below_args
         self.above_arg_list = above_args
         self.use_default = use_default
         self.default_color_map = default_color_map
+        self.trace_h = trace_h
+        self.above_filex = above_filex
+        self.above_decl = above_decl
+        self.below_filex = below_filex
+        self.below_decl = below_decl
 
 def list_args(args, root):
     #Each configurable arg has a set of possible choices it could be.
@@ -95,7 +116,19 @@ def name_and_args(tag, root):
         else:
             arg_list = None
 
-        return wrap_func(name, arg_list)
+        filex_tag = tag.find('file_extension')
+        if(filex_tag != None):
+            filex = filex_tag.text.strip()
+        else:
+            filex = None
+
+        decl_tag = tag.find('declaration')
+        if(decl_tag != None):
+            decl = decl_tag.text.strip()
+        else:
+            decl = None
+
+        return wrap_func(name, arg_list, filex, decl)
         
     else:
         return None
@@ -128,16 +161,21 @@ def parse_config(cmd_args):
         wrap_above_func = name_and_args(root.find('wrap_above_func'), root)
         default_color_map = {}
     else:
-        wrap_below_func = wrap_func('', [])
-        wrap_above_func = wrap_func('', [])
+        wrap_below_func = wrap_func('', [], '', '')
+        wrap_above_func = wrap_func('', [], '', '')
     
     c = config_class(\
                     wrap_above_func.name, \
                     wrap_above_func.arg_list, \
+                    wrap_above_func.filex, \
+                    wrap_above_func.decl,
                     wrap_below_func.name, \
                     wrap_below_func.arg_list, \
+                    wrap_below_func.filex, \
+                    wrap_below_func.decl, \
                     use_default, \
-                    default_color_map \
+                    default_color_map, \
+                    cmd_args.trace_h \
                     )
         
     return c
@@ -369,18 +407,29 @@ def wrap_the_call(c, call, config):
 
     return string
 
-    
+def trace_header(config): 
+    start = '#include "profile.h"'
+
+    between = ''
+    #If it's a .cpp file, just include the header files
+    if(config.above_filex == '.cpp' and config.below_filex == '.cpp'):
+        between += '\n'
+        for h in config.trace_h:
+            between += '#include "{h_file}"\n'.format(h_file = h)
+
+    #If it's a .c file, you have to declare the trace functions as extern
+    if(config.above_filex == '.c' and config.below_filex == '.c'):
+        between += 'extern "C" {decl};\n'.format(decl = config.above_decl)
+        between += 'extern "C" {decl};\n'.format(decl = config.below_decl)
+
+    end = 'extern class Profile profile;' + '\n\n' \
+          + 'using namespace std;' + '\n\n'
+
+    return start + '\n' + between + '\n' + end
+
 def write_hooks_cpp(f, core_kernel_list, config):
     #Write the top section
-    f.write( \
-    '''#include "profile.h"
-
-extern class Profile profile;
-
-using namespace std;
-
-''' \
-           )
+    f.write(trace_header(config))
 
     #Write all the extern "C" kernels
     for c in core_kernel_list.values():
@@ -393,12 +442,47 @@ using namespace std;
         string += wrap_the_call(c, call, config)
         f.write(string) 
 
+def cmake_add_library(cmd_args):
+
+    start = '''add_library(
+                          profile_lib SHARED'''
+
+    end =  '''
+           hooks.cpp
+           autogen.cpp
+           profile.cpp
+           )'''
+    
+    spaces = len('add_library') * ' '
+    
+    trace_files = ''
+    if(cmd_args.trace_c):
+        for f in cmd_args.trace_c:
+            trace_files += spaces + f + '\n'
+
+    call = start + '\n' + trace_files + spaces + end
+
+    return call
+
+def include_directories(cmd_args):
+    start = 'include_directories(' + '\n'
+    spaces = (len(start)-2) * ' '
+    end = ')'
+    between = spaces + cmd_args.dir + '/include' + '\n'
+    for d in cmd_args.include:
+        between += spaces + d + '\n'
+    return start + between + spaces + end
+
 #Start of main code
 
 #Create an argument parser
-parser = argparse.ArgumentParser(description='Options for autogeneration')
-parser.add_argument('-d', '--dir', required=True)
-parser.add_argument('-w', '--wrapper', required=True)
+parser = argparse.ArgumentParser(description='Create a CMakeLists.txt file and run CMake for PLASMA Tracer')
+
+parser.add_argument('-d', '--dir', help = 'Specify the PLASMA directory', required = True)
+parser.add_argument('-w', '--wrapper', help = 'Specify the XML wrapper config file', required = True)
+parser.add_argument('-i', '--include', help = 'Specify the directories containing the .h file for the two tracer functions', nargs = '*')
+parser.add_argument('-c', '--trace_c', help = 'Specify a list of .c/.cpp files to compile the two trace functions', nargs = '*')
+parser.add_argument('-r', '--trace_h', help = 'Specify which header files in the included directories are needed', nargs = '*')
 
 cmd_args = parser.parse_args()
 
@@ -439,3 +523,31 @@ hooks_cpp = open('hooks.cpp', 'w')
 write_hooks_cpp(hooks_cpp, core_kernel_list, config)
 
 hooks_cpp.close()
+
+cmake_lists = open('CMakeLists.txt', 'w')
+
+cmake_lists.write(
+"""set(CMAKE_CXX_COMPILER /Users/hhughe11/research/gcc_compiler/bin/g++-6.3.0)
+set(CMAKE_C_COMPILER /Users/hhughe11/research/gcc_compiler/bin/gcc-6.3.0)
+    
+{include_dirs}
+
+project(optimize C CXX)
+
+{add_library}
+
+target_link_libraries(
+                     profile_lib
+                     {plasma_dir}/lib/libcoreblas.so
+                     {plasma_dir}/lib/libplasma.so
+                     -fopenmp
+                     -flat_namespace
+                     -g
+                     )
+""".format(include_dirs = include_directories(cmd_args), plasma_dir = cmd_args.dir, add_library = cmake_add_library(cmd_args))
+)
+
+cmake_lists.close()
+
+subprocess.run(['cmake', '.'])
+subprocess.run(['make'])
