f86a95349567fba002364325550164ae20e05835
Harry <hhughe11@vols.utk.edu>
2017-12-08 00:12:31 -0500

    Added function generating capability into core_class in new_autogen.py

--- new_autogen.py
+++ new_autogen.py
@@ -27,20 +27,21 @@ class rgx_obj_class:
         self.file_regex = re.compile(r'core_blas_\w\.h')
         
         self.core_regex = re.compile(
-        
         r'\s*(\w*\n?)\s+(core_(?!omp)\w+)\s*\(([^;]*)\)')
         
         self.arg_regex = re.compile(r'(.*) \**(.*)')
 
 class core_class:
     
-    def __init__(self, rtype, name, args):
+    def __init__(self, rtype, name, args, mode_str):
         
         self.rtype = rtype
         
         self.name = name
         
-        self.fake_data_class = '' 
+        if(mode_str == 'autotune'):
+            
+            self.fake_data_class = '' 
 
         #Only a member variable for testing purposes
         self.typed_arg_list = self.extract_typed_args(args)
@@ -55,45 +56,136 @@ class core_class:
         self.definition_string = self.generate_definition_string()
         
         #Only a member variable for testing purposes
-        self.function_call_string = self.generate_function_call_string()
+        self.function_call_str = self.generate_function_call_string(mode_str)
+
+        #express the typedef as a list
+        self.typedef_as_list = self.listify_function(self.typed_arg_list,\
+                                                     self.typedef_string)
+
+        #express the typedef as a string
+        self.typedef = self.space_out(self.typedef_as_list, '')
+
+        #express the function definition as a list
+        self.function_def_as_list = self.listify_function(self.typed_arg_list,\
+                                                          self.definition_string)
+
+        #express the function definition as a string
+        self.function_def = self.space_out(self.function_def_as_list, '')
+
+        #express the function call as a list
+        self.function_call_as_list = self.listify_function(self.untyped_arg_list,\
+                                                           self.function_call_str)
+
+        #express the function call as a string for testing purposes
+        self.function_call = self.space_out(self.function_call_as_list, '    ')
 
     def generate_typedef_string(self):
         
-        string = ''
-
+        string = 'typedef {rtype} (*{name}_hook_type)(\n'\
+                 .format(rtype = self.rtype,\
+                         name = self.name)
         return string
+
+    def generate_function_call_string(self, mode_str):
+
+        if(mode_str == 'trace'):
+            
+            if(self.rtype == 'void'):
+
+                call = '(({name}_hook_type)profile.core[{upper}])(\n'\
+                .format(name = self.name, upper = self.name.upper())
+
+            else:
+
+                call = 'ret_val = (({name}_hook_type)profile.core[{upper}])(\n'\
+                .format(name = self.name, upper = self.name.upper())
+
+        elif(mode_str == 'autotune'):
+
+            if(self.rtype == 'void'):
+                
+                call = '(({name}_hook_type)autotune.core[{upper}])(\n'\
+                .format(name = self.name, upper = self.name.upper())
+
+            else:
+                
+                call = 'ret_val = (({name}_hook_type)autotune.core[{upper}])(\n'\
+                .format(name = self.name, upper = self.name.upper())
+
+        return call
 
     def generate_definition_string(self):
-
-        string = ''
-
-        return string
-
-    def generate_function_call_string(self):
         
-        string = ''
+        string = 'extern "C" {rtype} {name}(\n'.format(rtype = self.rtype,\
+                                                       name = self.name)
 
         return string
+
+    #concatenate lines and spaces into a string
+    def space_out(self, lines, spaces):
+        
+        full_string = ''
+
+        for line in lines:
+
+            string = spaces + line
+
+            full_string += string
+
+        return full_string
 
     #create each line of the function as a list element properly spaced
-    #call_string is the output of one of the generate_string functions above
-    def listify_function(arg_list, call_string):
+    def listify_function(self, arg_list, call_string):
         
         f = []
 
+        spaces = (len(call_string) - 2) * ' '
+
         f.append(call_string)
+
+        #Does not include final arg
+        for arg in arg_list[:-1]:
+            
+            f.append(spaces + arg + ',\n')
+
+        #final arg is not followed by a comma
+        f.append(spaces + arg_list[-1] + '\n')
+
+        #closing paren
+        f.append(spaces + ');\n')
 
         return f
     
     def print_lists(self):
 
-        print('typed_arg_list for {cname}:'.format(cname = self.name))
+        print('{cname}:'.format(cname = self.name))
+
+        print('typedef: {typedef}'.format(typedef = self.typedef_string))
+
+        print('full typedef:')
+
+        print(self.typedef)
+
+        print('function_call: {call}'.format(call = self.function_call_str))
+
+        print('full function call:')
+
+        print(self.function_call)
+        
+        print('function_definition: {d}'\
+              .format(d = self.definition_string))
+
+        print('full function definition:')
+
+        print(self.function_def)
+
+        print('typed args:')
 
         for typed_arg in self.typed_arg_list:
 
             print('arg: {arg}'.format(arg = typed_arg))
 
-        print('untyped_arg_list for {cname}'.format(cname = self.name))
+        print('untyped args:')
 
         for untyped_arg in self.untyped_arg_list:
             
@@ -151,7 +243,7 @@ def dump_file(string):
     
     return file_string
 
-def parse_hdr(hdr_file_string):
+def parse_hdr(hdr_file_string, mode_str):
     
     core_kernel_list = {}
 
@@ -163,7 +255,8 @@ def parse_hdr(hdr_file_string):
             
             core_kernel_list[m.group(2)] = core_class(m.group(1),\
                                                       m.group(2),\
-                                                      m.group(3))
+                                                      m.group(3),\
+                                                      mode_str)
 
     return core_kernel_list
 
@@ -181,8 +274,18 @@ tree = xml.parse(parser.parse_args().config)
 
 root = tree.getroot()
 
-#Open autogen_types.h
-autogen_types_h = open('autogen_types.h', 'w')
+#Determine operating mode
+mode_tag = root.find('mode')
+
+if(mode_tag != None):
+
+    mode_str = mode_tag.text.strip()
+
+else:
+
+    print("No mode tag found. Exiting.")
+    
+    sys.exit()
 
 #Create a regex object
 rgx_obj = rgx_obj_class()
@@ -201,7 +304,7 @@ for f in p.iterdir():
         hdr_file_string += dump_file(str(f))
 
 #Create kernel list
-core_kernel_list = parse_hdr(hdr_file_string)
+core_kernel_list = parse_hdr(hdr_file_string, mode_str)
 
 #Test code
 for node in core_kernel_list.values():
