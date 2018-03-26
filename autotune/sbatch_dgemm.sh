4e182b083603f4e54e522bac78151cc13f7cc15a
Harry Hughes <hhughe11@saturn.icl.utk.edu>
2018-03-26 13:23:59 -0400

    Added batch file to get dgemm points.

---
+++ autotune/sbatch_dgemm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#SBATCH --job-name=point_select
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=12:00:00
+#SBATCH --time-min=12:00:00
+#SBATCH --nodes=1
+#SBATCH --output=sbatch_stdout.txt
+#SBATCH --nodelist=a01
+#SBATCH --partition=shared
+
+export OMP_NUM_THREADS=20
+
+./autotune_raw.bin dgemm 5000 0 5000 0 2 -1 -1 < tile_sizes_stdin.txt
